ebed7c2d4a803e3dbbed45a9d694585af1cca5de
DithyaAnil <adithyaanil002@gmail.com>
2024-09-27 19:20:57 +0530

    Data Caching

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
 import "./App.css";
-import axios from "axios";
+import Customers from "./components/Customers";
+import { useState } from "react";
 
 function App() {
-  const customerQuery = useQuery({
-    queryKey: ["customers"],
-    queryFn: () => {
-      return axios("http://localhost:3000/api/customers");
-    },
-    refetchOnWindowFocus: false,
-    refetchInterval: 1000,
-    refetchIntervalInBackground: false,
-  });
-
-  if (customerQuery?.data?.data?.customers) {
-    return (
-      <div className="App">
-        {customerQuery.data.data.customers.map((customer) => {
-          return <p key={customer._id}>{customer.name}</p>;
-        })}
-      </div>
-    );
-  }
+  const [showCustomers, setShowCustomers] = useState(false);
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setShowCustomers(!showCustomers);
+        }}
+      >
+        {showCustomers ? "Hide customers bro" : "Show customers bro"}
+      </button>
+      {showCustomers ? <Customers /> : null}
+    </div>
+  );
 }
 
 export default App;
