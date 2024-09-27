5ee2b2f2fb190a59f80d3804ea21b819f63cc45d
DithyaAnil <adithyaanil002@gmail.com>
2024-09-27 15:35:34 +0530

    Starting React-Query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "@tanstack/react-query";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["test"],
+    queryFn: () => {
+      return axios(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+    },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error ? <p>Uh oh!Error</p> : null}
+      {isLoading ? <p>Loading...</p> : null}
+      {data?.data?.bitcoin?.usd ? data.data.bitcoin.usd : null}
     </div>
   );
 }
